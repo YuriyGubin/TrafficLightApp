678e72db7e4f641de64d5edd13037c3745404995
GubinYuriy <iosdevelop.2022@gmail.com>
2022-09-16 16:01:33 +0500

    Setup views

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var redView: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        greenView.layer.cornerRadius = 60
+        yellowView.layer.cornerRadius = 60
+        redView.layer.cornerRadius = 60
+        switchButton.layer.cornerRadius = 10
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
     }
 
-
+    @IBAction func switchButtonTapped() {
+        if switchButton.titleLabel?.text == "Start" {
+            greenView.alpha = 1
+            switchButton.setTitle("Next", for: .normal)
+        }
+        
+        if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            switchButton.setTitle("Next", for: .normal)
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            switchButton.setTitle("Next", for: .normal)
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+            switchButton.setTitle("Next", for: .normal)
+        }
+    }
+    
 }
 
